02eb9c75eef4c121c16f8a51f2a1a909945db874
Aristone <theariston@gmail.com>
2015-06-01 13:16:21 -0500

    first commit

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -2,9 +2,9 @@
 
 var Promise = require('es6-promise').Promise
 // just Node?
-// var fetch = require('node-fetch')
+var fetch = require('node-fetch')
 // Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+require('whatwg-fetch') //--> not a typo, don't store as a var
 
 // es6 polyfills, powered by babel
 require("babel/register")
@@ -21,4 +21,33 @@ require("babel/register")
     // start app
     // new Router()
 // }
+
+
+var urls = [ 'https://api.github.com/users/Aristone','https://api.github.com/users/Aristone/repos' ]
+
+var requests = urls.map((url) => fetch(url).then((r) => r.json()))
+
+function qs(selector) {
+    return document.querySelector(selector)
+    console.log(selector)
+}
+
+Promise.all(requests).then((data) => {
+    var profile = data[0],
+        repos = data[1]
+
+    var profile_string = ['name', 'login', 'blog', 'location', 'email', 'html_url'].map((key) => `<li>${key}: ${profile[key]}</li>`).join('')
+    var repo_string = repos.map((repo) => `<li><a href="${repo.html_url}">${repo.name}</a></li>`).join('')
+
+    qs('.profile img').src = profile.avatar_url
+    qs('.profile ul').innerHTML = profile_string
+    qs('.repos ul').innerHTML = repo_string
+})
+
+
+
+
+
+
+
 
